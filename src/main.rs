2261bcadf4c13d0c67732a8bfb21c7947489ea04
allspike <jspikerman@icloud.com>
2025-08-09 15:47:03 -0700

    Added two new files. Main contains the main function and the main.rs file. cli.rs contains the test functions. Cargo.toml was updated to include new dependencies and features.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author = "Justin Spikerman <justin@spikerman.net>", version, about)]
+/// Rust version of the 'echo' command
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
 }
